696987e8f8d8d85a7d9a67d5a946dbe8881fdaa9
FIRST_NAME LAST_NAME <sushank123456@gmail.com>
2021-12-31 14:55:13 +0530

    Initial endpoint

---
+++ URLFeederService/src/main/java/com/github/kaushiknishant/URLFeederService/UrlFeederServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.kaushiknishant.URLFeederService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlFeederServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlFeederServiceApplication.class, args);
+	}
+
+}
---
+++ URLFeederService/src/test/java/com/github/kaushiknishant/URLFeederService/UrlFeederServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kaushiknishant.URLFeederService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlFeederServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
